47abdec09e86c7cb8f6a3dc57229ee6f9e257b51
LincolnRP01 <lincolnsuper0@gmail.com>
2023-12-18 11:06:53 -0300

    Criando novo desafio

--- Desafio-do-felipao/index.js
+++ Desafio-do-felipao/index.js
@@ -1,3 +1,27 @@
+let nickname = prompt("Qual o nome do seu heroi?")
+let exp = prompt("Qual nivel de xp que seu heroi possui?(1 a 10001)")
+
+if(exp <= 1000){
+    exp = "Nivel Ferro"
+}else if(exp >= 1001 && exp <= 2000){
+    exp = "Nivel Bronze"
+}else if(exp >= 2001 && exp <= 6000){
+    exp = "Nivel Prata"
+}else if(exp >= 6001 && exp <= 7000){
+    exp = "Nivel Ouro"
+}else if(exp >= 7001 && exp <= 8000){
+    exp = "Nivel Platina"
+}else if(exp >= 8001 && exp <= 9000){
+    exp = "Nivel Ascentente"
+}else if(exp >= 9001 && exp <= 10000){
+    exp = "Nivel Imortal"
+}else{
+    exp = "Nivel Radiant"
+}
+
+alert(`O Herói de nome: ${nickname} está no ${exp}`)
+
+/*
 let nickname = prompt("Qual o nome do seu herói?");
 let exp = parseInt(prompt("Qual nível de XP que seu herói possui? (1 a 10001)"));
 
@@ -30,29 +54,6 @@ switch (true) {
 }
 
 alert(`O Herói de nome: ${nickname} está no ${level}`);
-
-
-/*
-let nickname = prompt("Qual o nome do seu heroi?")
-let exp = prompt("Qual nivel de xp que seu heroi possui?(1 a 10001)")
-
-if(exp <= 1000){
-    exp = "Nivel Ferro"
-}else if(exp >= 1001 && exp <= 2000){
-    exp = "Nivel Bronze"
-}else if(exp >= 2001 && exp <= 6000){
-    exp = "Nivel Prata"
-}else if(exp >= 6001 && exp <= 7000){
-    exp = "Nivel Ouro"
-}else if(exp >= 7001 && exp <= 8000){
-    exp = "Nivel Platina"
-}else if(exp >= 8001 && exp <= 9000){
-    exp = "Nivel Ascentente"
-}else if(exp >= 9001 && exp <= 10000){
-    exp = "Nivel Imortal"
-}else{
-    exp = "Nivel Radiant"
-}
-
-alert(`O Herói de nome: ${nickname} está no ${exp}`)
 */
+
+
